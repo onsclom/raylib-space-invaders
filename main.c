a2be3612cb1b9e7a0341ccf44bbd2c01400858a7
Austin Merrick <austinmerrick@onsclom.net>
2026-04-23 19:44:55 -0700

    refactor

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -14,21 +13,18 @@
 #define ENEMY_SIZE 20.0
 
 typedef struct {
-  Vector2 position;
-  Vector2 size;
+  Rectangle rect;
   bool alive;
 } Enemy;
 
 typedef struct {
-  Vector2 position;
-  Vector2 size;
+  Rectangle rect;
   Vector2 velocity;
   bool alive;
 } Bullet;
 
 typedef struct {
-  Vector2 player_position; // top left of player
-  Vector2 player_size;
+  Rectangle player;
 
   Bullet bullets[MAX_BULLET_AMOUNT];
   size_t next_bullet;
@@ -41,15 +37,15 @@ typedef struct {
 
   bool game_over;
 
+  size_t alive_enemies;
   Enemy enemies[ENEMY_ROWS * ENEMY_COLS];
 } GameState;
 
 const int game_width = 400;
 const int game_height = 400;
-const float player_height = 50.0f;
-GameState state = {
-    .player_position = {0, game_height - player_height},
-    .player_size = {50.0f, player_height},
+const float player_height = 50.0;
+static GameState state = {
+    .player = {0.0f, (float)game_height - player_height, 50.0f, player_height},
     .next_bullet = 0,
     .step_size = 15.0,
     .step_size_y = 25.0,
@@ -57,142 +53,102 @@ GameState state = {
     .step_interval = 0.25,
     .step_acc = 0.0,
     .game_over = false,
+    .alive_enemies = ENEMY_AMOUNT,
 };
 
 int main(void) {
-  InitWindow(game_width, game_height, "space invaders");
-
-  {
-    // init enemies
-    for (size_t i = 0; i < ENEMY_AMOUNT; i++) {
-      const float row = (float)(i / ENEMY_COLS);
-      const float col = (float)(i % ENEMY_COLS);
-      Enemy *cur_enemy = &state.enemies[i];
-      cur_enemy->position.x = ENEMY_SIZE * col + (col * ENEMY_SPACING);
-      cur_enemy->position.y = ENEMY_SIZE * row + (row * ENEMY_SPACING);
-      cur_enemy->size.x = ENEMY_SIZE;
-      cur_enemy->size.y = ENEMY_SIZE;
-      cur_enemy->alive = true;
-    }
+  // init enemies
+  for (size_t i = 0; i < ENEMY_AMOUNT; i++) {
+    const float row = (float)(i / ENEMY_COLS);
+    const float col = (float)(i % ENEMY_COLS);
+    state.enemies[i].rect = (Rectangle){
+        .x = (float)ENEMY_SIZE * col + (col * ENEMY_SPACING),
+        .y = (float)ENEMY_SIZE * row + (row * ENEMY_SPACING),
+        .width = (float)ENEMY_SIZE,
+        .height = (float)ENEMY_SIZE,
+    };
+    state.enemies[i].alive = true;
   }
+
+  InitWindow(game_width, game_height, "space invaders");
 
   while (!WindowShouldClose()) {
     const float dt = GetFrameTime();
 
-    BeginDrawing();
-
     // UPDATE
     /////////////////
 
-    int dir_x = 0;
-
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-      dir_x--;
-    }
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-      dir_x++;
+    {
+      int dir_x = 0;
+      if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+        dir_x--;
+      }
+      if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+        dir_x++;
+      }
+      const float player_speed = 250.0f;
+      state.player.x += (float)dir_x * dt * player_speed;
+      state.player.x = Clamp(state.player.x, 0.0f,
+                             (float)game_width - state.player.width);
     }
 
     if (IsKeyPressed(KEY_SPACE)) {
       Bullet *new_bullet = &state.bullets[state.next_bullet];
-      new_bullet->position.y = state.player_position.y;
-      new_bullet->size.x = 10;
-      new_bullet->size.y = 10;
-      const float BULLET_SPEED = 250.0f;
-      new_bullet->velocity.y = -1 * BULLET_SPEED;
-
-      new_bullet->position.x = state.player_position.x +
-                               state.player_size.x * .5f -
-                               new_bullet->size.x * .5f;
-
+      new_bullet->rect.width = 10;
+      new_bullet->rect.height = 10;
+      new_bullet->rect.y = state.player.y;
+      new_bullet->rect.x = state.player.x + state.player.width * .5f -
+                           new_bullet->rect.width * .5f;
+      const float bullet_speed = 250.0f;
+      new_bullet->velocity.y = -1 * bullet_speed;
       new_bullet->alive = true;
-
       state.next_bullet++;
       state.next_bullet %= MAX_BULLET_AMOUNT;
     }
 
-    // TODO: put enemy updating here!
     state.step_acc += dt;
     while (state.step_acc >= state.step_interval) {
       state.step_acc -= state.step_interval;
-      // advance all enemy positions by some step size in some direction
+      float amount_to_move = state.step_size * (float)state.step_dir;
 
-      float amount_to_move = state.step_size * state.step_dir;
+      float min_x = (float)game_width;
+      float max_x = 0.0f;
+      for (size_t j = 0; j < ENEMY_AMOUNT; j++) {
+        Enemy *curr_enemy = &state.enemies[j];
+        if (!curr_enemy->alive) {
+          continue;
+        }
+        if (curr_enemy->rect.x < min_x) {
+          min_x = curr_enemy->rect.x;
+        }
+        if (curr_enemy->rect.x > max_x) {
+          max_x = curr_enemy->rect.x;
+        }
+      }
 
-      if (state.step_dir > 0) {
+      float dist_to_wall = (state.step_dir > 0)
+                               ? (float)game_width - (max_x + (float)ENEMY_SIZE)
+                               : min_x;
 
-        float max_x = 0.0;
-
+      if (dist_to_wall <= state.step_size) {
+        amount_to_move = (float)state.step_dir * dist_to_wall;
+        state.step_dir = -state.step_dir;
         for (size_t j = 0; j < ENEMY_AMOUNT; j++) {
           Enemy *curr_enemy = &state.enemies[j];
-          bool alive = curr_enemy->alive;
-
-          if (!alive) {
+          if (!curr_enemy->alive) {
             continue;
           }
-
-          if (curr_enemy->position.x > max_x) {
-            max_x = curr_enemy->position.x;
-          }
-        }
-
-        if (game_width - (max_x + ENEMY_SIZE) <= state.step_size) {
-          amount_to_move = (game_width - (max_x + ENEMY_SIZE));
-          state.step_dir = -1;
-
-          // move all enemies down by step_size_y
-          for (size_t j = 0; j < ENEMY_AMOUNT; j++) {
-            Enemy *curr_enemy = &state.enemies[j];
-            bool alive = curr_enemy->alive;
-
-            if (!alive) {
-              continue;
-            }
-
-            curr_enemy->position.y += state.step_size_y;
-          }
-        }
-      } else {
-        float min_x = game_width;
-
-        for (size_t j = 0; j < ENEMY_AMOUNT; j++) {
-          Enemy *curr_enemy = &state.enemies[j];
-          bool alive = curr_enemy->alive;
-
-          if (!alive) {
-            continue;
-          }
-
-          if (curr_enemy->position.x < min_x) {
-            min_x = curr_enemy->position.x;
-          }
-        }
-
-        if (min_x - state.step_size <= 0) {
-          amount_to_move = -min_x;
-          state.step_dir = 1;
-
-          // move all enemies down by step_size_y
-          for (size_t j = 0; j < ENEMY_AMOUNT; j++) {
-            Enemy *curr_enemy = &state.enemies[j];
-            bool alive = curr_enemy->alive;
-
-            if (!alive) {
-              continue;
-            }
-
-            curr_enemy->position.y += state.step_size_y;
-          }
+          curr_enemy->rect.y += state.step_size_y;
         }
       }
 
       for (size_t i = 0; i < ENEMY_AMOUNT; i++) {
         Enemy *curr_enemy = &state.enemies[i];
 
-        curr_enemy->position.x += amount_to_move;
+        curr_enemy->rect.x += amount_to_move;
 
         // check if an enemy is at player height
-        if (curr_enemy->position.y + ENEMY_SIZE >= state.player_position.y) {
+        if (curr_enemy->rect.y + curr_enemy->rect.height >= state.player.y) {
           state.game_over = true;
         }
       }
@@ -200,65 +156,34 @@ int main(void) {
 
     for (size_t i = 0; i < MAX_BULLET_AMOUNT; i++) {
       Bullet *cur_bullet = &state.bullets[i];
-      const bool alive = cur_bullet->alive;
-
-      if (!alive) {
+      if (!cur_bullet->alive) {
         continue;
       }
 
-      cur_bullet->position.y += cur_bullet->velocity.y * dt;
+      cur_bullet->rect.y += cur_bullet->velocity.y * dt;
 
-      for (size_t i = 0; i < ENEMY_AMOUNT; i++) {
-        Enemy *cur_enemy = &state.enemies[i];
+      for (size_t j = 0; j < ENEMY_AMOUNT; j++) {
+        Enemy *cur_enemy = &state.enemies[j];
         if (!cur_enemy->alive) {
           continue;
         }
 
-        const float enemy_center_x =
-            cur_enemy->position.x + .5f * cur_enemy->size.x;
-        const float bullet_center_x =
-            cur_bullet->position.x + .5f * cur_bullet->size.x;
-        const float x_dist = enemy_center_x - bullet_center_x;
-        const bool colliding_in_x = fabsf(x_dist) <= (cur_enemy->size.x * .5f +
-                                                      cur_bullet->size.x * .5f);
-
-        const float enemy_center_y =
-            cur_enemy->position.y + .5f * cur_enemy->size.y;
-        const float bullet_center_y =
-            cur_bullet->position.y + .5f * cur_bullet->size.y;
-        const float y_dist = enemy_center_y - bullet_center_y;
-        const bool colliding_in_y = fabsf(y_dist) <= (cur_enemy->size.y * .5f +
-                                                      cur_bullet->size.y * .5f);
-
-        const bool colliding = colliding_in_x && colliding_in_y;
-
-        if (colliding) {
+        if (CheckCollisionRecs(cur_bullet->rect, cur_enemy->rect)) {
           cur_enemy->alive = false;
           cur_bullet->alive = false;
+          state.alive_enemies--;
         }
-      }
-    }
-
-    const float player_speed = 250.0f;
-    state.player_position.x += dir_x * dt * player_speed;
-
-    state.player_position.x =
-        Clamp(state.player_position.x, 0, game_width - state.player_size.y);
-
-    bool allDead = true;
-    for (size_t i = 0; i < ENEMY_AMOUNT; i++) {
-      if (state.enemies[i].alive) {
-        allDead = false;
-        break;
       }
     }
 
     // DRAW
     /////////////////
 
+    BeginDrawing();
+
     ClearBackground(RAYWHITE);
 
-    DrawRectangleV(state.player_position, state.player_size, BLUE);
+    DrawRectangleRec(state.player, BLUE);
 
     // DRAW ALL BULLETS
     for (size_t i = 0; i < MAX_BULLET_AMOUNT; i++) {
@@ -266,7 +191,7 @@ int main(void) {
       if (!cur_bullet.alive) {
         continue;
       }
-      DrawRectangleV(cur_bullet.position, cur_bullet.size, RED);
+      DrawRectangleRec(cur_bullet.rect, RED);
     }
 
     // DRAW ALL ENEMIES
@@ -276,14 +201,14 @@ int main(void) {
         continue;
       }
 
-      DrawRectangleV(cur_enemy.position, cur_enemy.size, GREEN);
+      DrawRectangleRec(cur_enemy.rect, GREEN);
     }
 
     if (state.game_over) {
       DrawText("GAME OVER", 0, 0, 50.0, RED);
     }
 
-    if (allDead) {
+    if (state.alive_enemies == 0) {
       DrawText("YOU WIN", 0, 0, 50.0, GREEN);
     }
 
